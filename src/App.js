45b37007e7dc46b95d7e193063cf0aadfdd4caa9
m1k431 <garcia.mikaelgmail.com>
2020-10-31 19:19:32 +0100

    react-particles-js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import './css/app.css';
 import VideoJonglage from './jonglage';
 import JeuCv from './JeuCv'
 import Soleil from './soleilReact'
-
+import Particles from 'react-particles-js'
 
 function App() {
   return (
     <div className="App">
       <div className="Body">
+        <Particles id='Particles' params={{
+          fpsLimit: 60,
+          backgroundMode: {
+            enable: true,
+            zIndex: -1
+          },
+          "particles": {
+            "number": {
+              "value": 40
+            },
+            "size": {
+              "value": 1
+            }
+          },
+          "interactivity": {
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "repulse"
+              }
+            }
+          }
+        }} />
         <VideoJonglage></VideoJonglage>
         <JeuCv></JeuCv>
         <Soleil></Soleil>
