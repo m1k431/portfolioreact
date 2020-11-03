d08b23361520bb1f3abae9c905f521b9ac6c9bd5
m1k431 <garcia.mikaelgmail.com>
2020-11-03 12:16:59 +0100

    externalisation objet params Particles

--- src/App.js
+++ src/App.js
@@ -6,35 +6,37 @@ import JeuCv from './JeuCv'
 import Soleil from './soleil'
 import Particles from 'react-particles-js'
 
+const paramParticles = {
+  fpsLimit: 60,
+  backgroundMode: {
+    enable: true,
+    zIndex: -1
+  },
+  "particles": {
+    "number": {
+      "value": 80
+    },
+    "size": {
+      "value": 1
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
-        <Particles id='Particles' params={{
-          fpsLimit: 60,
-          backgroundMode: {
-            enable: true,
-            zIndex: -1
-          },
-          "particles": {
-            "number": {
-              "value": 80
-            },
-            "size": {
-              "value": 1
-            }
-          },
-          "interactivity": {
-            "events": {
-              "onhover": {
-                "enable": true,
-                "mode": "repulse"
-              }
-            }
-          }
-        }} />
-        <VideoJonglage></VideoJonglage>
-        <JeuCv></JeuCv>
-        <Soleil></Soleil>
+      <Particles id='Particles' params={paramParticles} />
+      <VideoJonglage></VideoJonglage>
+      <JeuCv></JeuCv>
+      <Soleil></Soleil>
     </div>
   );
 }
